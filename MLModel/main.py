c9d700af99867dfe074107ae3c68b583f4a2d2e2
Simagine/Sadek-Amine <cscience670@gmail.com>
2026-07-11 23:55:52 +0100

    Clean up main.py with consistent formatting

--- MLModel/main.py
+++ MLModel/main.py
@@ -6,5 +6,5 @@ data = pd.read_csv("creditcard.csv")
 # iju(hiu(hoijh(('hjgjgbkjbgilkukgjugiuhilhlohloioعرض أول 5 صفوف
 print(data.head())
 yuhtuihtiuriojio-bhiuoyuhovghfyufyuuiui
-yufiuhiohgiutyiuopjiلبلالبللللللللللللللللللللu_
+yufiuhiohgiutyiuopjiلبلالبللللللللللللللللللللu_gggggggggggggggggefhryf
 hyugyjguihiojiojpoop
